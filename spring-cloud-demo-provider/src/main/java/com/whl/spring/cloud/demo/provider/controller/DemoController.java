d6a58d2d7022a2b78a5c43bc9eae8abe7766ec94
王洪亮 <w505957125@aliyun.com>
2023-05-23 12:59:40 +0800

    add sentinel

--- spring-cloud-demo-provider/src/main/java/com/whl/spring/cloud/demo/provider/controller/DemoController.java
+++ spring-cloud-demo-provider/src/main/java/com/whl/spring/cloud/demo/provider/controller/DemoController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
@@ -11,15 +14,25 @@ import jakarta.servlet.http.HttpServletResponse;
 @RequestMapping("/demo")
 public class DemoController {
 
+    @SentinelResource(value = "demoController#index", blockHandler = "handlerIndex")
     @GetMapping(value = {"", "/"})
     public String index() {
         return "Demo";
     }
 
+    @SentinelResource(value = "demoController#test", blockHandler = "handlerTest")
     @GetMapping("/test")
     public String test(HttpServletRequest request, HttpServletResponse response) {
         response.setHeader("test", "test");
         return request.getServletContext().getRealPath("");
+    }
+
+    public String handlerIndex(BlockException e) {
+        return "/demo: 请求过于频繁，请稍后再试。";
+    }
+
+    public String handlerTest(HttpServletRequest request, HttpServletResponse response, BlockException e) {
+        return "/demo/test: 请求过于频繁，请稍后再试。";
     }
 
 }
--- spring-cloud-demo-provider/src/main/java/com/whl/spring/cloud/demo/provider/rpc/impl/DemoServiceImpl.java
+++ spring-cloud-demo-provider/src/main/java/com/whl/spring/cloud/demo/provider/rpc/impl/DemoServiceImpl.java
@@ -7,24 +7,41 @@ import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.commons.lang3.RandomUtils;
 import org.springframework.stereotype.Service;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.whl.spring.cloud.demo.DemoService;
 
 @Service("demoServiceImpl")
 public class DemoServiceImpl implements DemoService {
 
+    @SentinelResource(value = "demoService#test", blockHandler = "handlerTest")
     @Override
     public String test(String name) {
         return "Hello " + name + ", this time is " + new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date());
     }
 
+    @SentinelResource(value = "demoService#test2", blockHandler = "handlerTest2")
     @Override
     public String test2(String name) {
         return "Hello " + name + ", random long is " + RandomUtils.nextLong();
     }
 
+    @SentinelResource(value = "demoService#test3", blockHandler = "handlerTest3")
     @Override
     public String test3(String name) {
         return "Hello " + name + ", random string is " + RandomStringUtils.randomAlphanumeric(10);
+    }
+
+    public String handlerTest(String name, BlockException e) {
+        return "DemoService#test: 请求过于频繁，请稍后再试。";
+    }
+
+    public String handlerTest2(String name, BlockException e) {
+        return "DemoService#test2: 请求过于频繁，请稍后再试。";
+    }
+
+    public String handlerTest3(String name, BlockException e) {
+        return "DemoService#test3: 请求过于频繁，请稍后再试。";
     }
 
 }
