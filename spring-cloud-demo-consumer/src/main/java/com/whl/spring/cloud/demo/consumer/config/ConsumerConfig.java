cd1a4c75c89b09662d38298131d4ac14a9694e6d
王洪亮 <w505957125@aliyun.com>
2024-01-25 11:42:25 +0800

    1. add RestClient demo

--- spring-cloud-demo-consumer/src/main/java/com/whl/spring/cloud/demo/consumer/config/ConsumerConfig.java
+++ spring-cloud-demo-consumer/src/main/java/com/whl/spring/cloud/demo/consumer/config/ConsumerConfig.java
@@ -4,6 +4,7 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 import com.alibaba.cloud.sentinel.annotation.SentinelRestTemplate;
@@ -16,6 +17,11 @@ public class ConsumerConfig {
     @SentinelRestTemplate(blockHandler = "handleException", blockHandlerClass = ExceptionUtil.class, fallback = "handleFallback", fallbackClass = ExceptionUtil.class)
     public RestTemplate restTemplate(RestTemplateBuilder builder) {
         return builder.build();
+    }
+
+    @Bean
+    public RestClient restClient(RestTemplate restTemplate) {
+        return RestClient.builder(restTemplate).build();
     }
 
 }
--- spring-cloud-demo-consumer/src/main/java/com/whl/spring/cloud/demo/consumer/controller/DemoController.java
+++ spring-cloud-demo-consumer/src/main/java/com/whl/spring/cloud/demo/consumer/controller/DemoController.java
@@ -7,10 +7,13 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestTemplate;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/demo")
@@ -21,6 +24,9 @@ public class DemoController {
 
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private RestClient restClient;
 
     @GetMapping(value = {"", "/"})
     public String index() throws Exception {
@@ -37,6 +43,17 @@ public class DemoController {
         String value = entity.getHeaders().getFirst("test");
         response.setHeader("test", value);
         return entity.getBody();
+    }
+
+    @GetMapping("/test2")
+    public String test2(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        String path = String.format("http://%s/demo/test", SERVICE_NAME);
+        logger.info("request path: {}", path);
+        return this.restClient.get().uri(path).exchange((req, resp) -> {
+            String value = resp.getHeaders().getFirst("test");
+            response.setHeader("test", value);
+            return Objects.requireNonNull(resp.bodyTo(String.class));
+        });
     }
 
     @GetMapping("/testRestTemplateDegrade")
